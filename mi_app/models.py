a8f47f84c8cbb0e9e388838c91a1aa39a5e4d505
yocaniba <elporte86@hotmail.com>
2026-01-21 17:57:01 -0300

    modificacion tabla bd

--- mi_app/models.py
+++ mi_app/models.py
@@ -6,7 +6,7 @@ class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    telefino = models.CharField(max_length=20)
+    telefono = models.CharField(max_length=20)
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
